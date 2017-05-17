363e0ab473b2cb3805dec00cf8bf2eb2a12a2320
stacywang0601 <stacywang61@gmail.com>
2017-05-17 15:24:15 -0400

    add View Resolver in todo-servlet.xml and jump to login.jsp

---
+++ webapp/src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -0,0 +1,14 @@
+package com.in28minutes.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	// ResponseBody--return directly instead of looking for url
+	// @ResponseBody
+	@RequestMapping(value = "/login")
+	public String sayHello() {
+		return "login";
+	}
+}
