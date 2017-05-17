d93122bd015d44ca453a450635076edb7889914f
stacywang0601 <stacywang61@gmail.com>
2017-05-17 15:59:19 -0400

    Use ModelMap model, POST/GET, show @RequestParam String name, show name on welcome page

--- webapp/src/main/java/com/in28minutes/springmvc/LoginController.java
+++ webapp/src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -1,14 +1,26 @@
 package com.in28minutes.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	// ResponseBody--return directly instead of looking for url
 	// @ResponseBody
-	@RequestMapping(value = "/login")
-	public String sayHello() {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage() {
 		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	// pass parameter in
+	public String handleUserLogin(ModelMap model, @RequestParam String name,
+			@RequestParam String password) {
+		model.put("name", name);
+		model.put("password", password);
+		return "welcome";
 	}
 }
