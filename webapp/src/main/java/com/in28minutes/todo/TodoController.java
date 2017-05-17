c7b02943e0334ef5d4072cd7ecc64c7387af6b01
stacywang0601 <stacywang61@gmail.com>
2017-05-17 18:26:43 -0400

    Add Facility to add New Todo, todo.jsp, model.clear(), redirect:/list-todos

--- webapp/src/main/java/com/in28minutes/todo/TodoController.java
+++ webapp/src/main/java/com/in28minutes/todo/TodoController.java
@@ -1,10 +1,13 @@
 package com.in28minutes.todo;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.todo.service.TodoService;
@@ -21,5 +24,19 @@ public class TodoController {
 		String user = (String) model.get("name");
 		model.addAttribute("todos", service.retrieveTodos(user));
 		return "list-todos";
+	}
+
+	// first click
+	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
+	public String showTodoPage() {
+		return "todo";
+	}
+
+	// after add todo
+	@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+	public String addTodo(ModelMap model, @RequestParam String desc) {
+		service.addTodo((String) model.get("name"), desc, new Date(), false);
+		model.clear();// to prevent request parameter "name" to be passed
+		return "redirect:/list-todos";
 	}
 }
--- webapp/src/main/webapp/WEB-INF/views/list-todos.jsp
+++ webapp/src/main/webapp/WEB-INF/views/list-todos.jsp
@@ -5,5 +5,6 @@
 <body>
 	<H1>Your Todos</H1>
 	${todos}
+	 <a class="button" href="/add-todo">Add</a>
 </body>
 </html>
