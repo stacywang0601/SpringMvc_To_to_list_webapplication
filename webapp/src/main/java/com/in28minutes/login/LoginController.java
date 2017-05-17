d38ac727b882125d1554d22e8494cb9a896c8841
stacywang0601 <stacywang61@gmail.com>
2017-05-17 17:06:46 -0400

    Use Auto-wiring to wire LoginService.

--- webapp/src/main/java/com/in28minutes/login/LoginController.java
+++ webapp/src/main/java/com/in28minutes/login/LoginController.java
@@ -1,17 +1,17 @@
 package com.in28minutes.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.in28minutes.login.LoginService;
-
 @Controller
 public class LoginController {
-
-	private LoginService service = new LoginService();
+	// let springmvc to set an service
+	@Autowired
+	private LoginService service;
 
 	// ResponseBody--return directly instead of looking for url
 	// @ResponseBody
--- webapp/src/main/java/com/in28minutes/login/LoginService.java
+++ webapp/src/main/java/com/in28minutes/login/LoginService.java
@@ -1,6 +1,11 @@
 package com.in28minutes.login;
 
+import org.springframework.stereotype.Service;
+
+//tell springmvc to new a service
+@Service
 public class LoginService {
+
 	public boolean validateUser(String user, String password) {
 		return user.equalsIgnoreCase("in28Minutes") && password.equals("dummy");
 	}
