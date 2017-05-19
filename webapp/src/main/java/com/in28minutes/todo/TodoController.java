ecdd479934c22605e2402224de285604530d57a9
stacywang0601 <stacywang61@gmail.com>
2017-05-19 07:22:50 -0400

    initBinder method for targetDate field

--- webapp/src/main/java/com/in28minutes/todo/TodoController.java
+++ webapp/src/main/java/com/in28minutes/todo/TodoController.java
@@ -1,13 +1,17 @@
 package com.in28minutes.todo;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +27,13 @@ public class TodoController {
 	@Autowired
 	private TodoService service;
 
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, false));
+	}
+
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showLoginPage(ModelMap model, String name) {
 		String user = (String) model.get("name");
@@ -34,7 +45,6 @@ public class TodoController {
 	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
 	public String showAddTodoPage(ModelMap model) {
 		model.addAttribute("todo", new Todo());
-
 		return "todo";
 	}
 
@@ -43,7 +53,6 @@ public class TodoController {
 	public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
 		if (result.hasErrors())
 			return "todo";
-
 		service.addTodo((String) model.get("name"), todo.getDesc(), new Date(),
 				false);
 		model.clear();// to prevent request parameter "name" to be passed
--- webapp/src/main/webapp/WEB-INF/views/list-todos.jsp
+++ webapp/src/main/webapp/WEB-INF/views/list-todos.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <html>
 <head>
 <title>Todos for ${name}</title>
@@ -23,13 +24,12 @@
 				<c:forEach items="${todos}" var="todo">
 					<tr>
 						<td>${todo.desc}</td>
-						<td>${todo.targetDate}</td>
+						<td><fmt:formatDate pattern="dd/MM/yyyy"
+								value="${todo.targetDate}" /></td>
 						<td>${todo.done}</td>
-						<td>
-							<a type="button" class="btn btn-primary"
-							href="/update-todo?id=${todo.id}">Edit</a> 
-							<a type="button" class="btn btn-warning"
-							 href="/delete-todo?id=${todo.id}">Delete</a></td>
+						<td><a type="button" class="btn btn-primary"
+							href="/update-todo?id=${todo.id}">Edit</a> <a type="button"
+							class="btn btn-warning" href="/delete-todo?id=${todo.id}">Delete</a></td>
 					</tr>
 				</c:forEach>
 			</tbody>
