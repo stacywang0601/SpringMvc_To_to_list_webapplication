065e487acd8fc1b34ddb17a13f3bd62da494ec56
stacywang0601 <stacywang61@gmail.com>
2017-05-19 17:36:00 -0400

    add spring security

---
+++ webapp/src/main/java/com/in28minutes/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.in28minutes.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.inMemoryAuthentication().withUser("in28Minutes").password("dummy")
+				.roles("USER", "ADMIN");
+	}
+
+	// any url contains todo will be securied unless a user with USER ROLE have
+	// access, other user will be forward to login
+	// /login is exposed by spring security
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll()
+				.antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+				.formLogin();
+	}
+}
