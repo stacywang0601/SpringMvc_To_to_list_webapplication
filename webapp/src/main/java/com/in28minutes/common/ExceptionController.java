85576aa86888ca00172c7015949b5e3554e05fb2
stacywang0601 <stacywang61@gmail.com>
2017-05-19 20:32:56 -0400

    exception handling

---
+++ webapp/src/main/java/com/in28minutes/common/ExceptionController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//this class is applicable to all the controllers
+@ControllerAdvice
+@EnableWebMvc
+public class ExceptionController {
+
+	private Log logger = LogFactory.getLog(ExceptionController.class);
+
+	// global exception handling
+	@ExceptionHandler(value = Exception.class)
+	public String handleError(HttpServletRequest req, Exception exception) {
+		logger.error("Request: " + req.getRequestURL() + " raised " + exception);
+		return "error";
+	}
+}
